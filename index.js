bac290d99d306a60b8f2ffc5eb0b9a8eaab2aca3
Aleph-Phoebe <phbwangui47@gmail.com>
2024-06-30 12:43:10 +0300

    COMPLETED

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random=0+1;
+
+let num3=24;
+let num4=20;
+let mod = num3%num4;
+
+let array=(16, 20, 13, 5, 2, 14, 1);
+
+let max= Math.max (16, 20, 13, 5, 2, 14, 1);
